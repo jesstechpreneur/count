67e8687a0e1b78fad95e211aebc860515033fb5e
Jess <hi@jesstechpreneur.com>
2022-06-08 00:12:03 +0000

    first commit

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Counter = (props) => {
+	let initialValue = "000000";
+	let seconds = props.seconds.toString();
+	let icon = <i className="far fa-clock"></i>;
+	let result = (
+		initialValue.split("").slice(seconds.length).join("") + seconds).split("");
+	result.unshift(icon);
+
+	return (
+		<div className="time-wrapper">
+			{result.map((e, i) => {
+				return <div key={i}>{e}</div>;
+			})}
+		</div>
+	);
+};
+Counter.PropTypes = {
+	seconds: PropTypes.string,
+};
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,16 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seconds = 1; // What does each second represent
+
+const timer = setInterval(
+	() => {
+		if (seconds === 999999) {
+			// This function tells what happens when counter hits 999999
+			clearInterval(timer);
+		}
+		ReactDOM.render(<Home />, document.querySelector("#app")); // Calls the Home jsx to user front renders home every second
+		seconds++; //This will add 1 to the seconds
+	},
+	1000 //Will run every second
+);
